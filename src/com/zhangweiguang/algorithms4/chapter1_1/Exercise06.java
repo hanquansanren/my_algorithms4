7342ff45b63fc4a2431cbedfbe992285400b335c
W.G.Zhang <1243271316@qq.com>
2020-11-12 10:13:45 +0800

    finished Exercise06

---
+++ src/com/zhangweiguang/algorithms4/chapter1_1/Exercise06.java
@@ -0,0 +1,17 @@
+package com.zhangweiguang.algorithms4.chapter1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+//Fibonacci sequence
+public class Exercise06 {
+    public static void main(String[] args)
+    {
+        int f=0,g=1;
+        for(int i=0;i<=15;i++)
+        {
+            StdOut.println(f);
+            f=f+g;
+            g=f-g;
+        }
+    }
+
+}
