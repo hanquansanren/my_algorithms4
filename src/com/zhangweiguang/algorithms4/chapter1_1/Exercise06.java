2f4553e67e882a3d6bdbc200795de1ca2155a006
W.G.Zhang <1243271316@qq.com>
2020-11-12 10:25:27 +0800

    finished Exercise06'

--- src/com/zhangweiguang/algorithms4/chapter1_1/Exercise06.java
+++ src/com/zhangweiguang/algorithms4/chapter1_1/Exercise06.java
@@ -6,7 +6,7 @@ public class Exercise06 {
     public static void main(String[] args)
     {
         int f=0,g=1;
-        for(int i=0;i<=15;i++)
+        for(int i=0;i<=25;i++)
         {
             StdOut.println(f);
             f=f+g;
