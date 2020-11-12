85b04395b7a17378d04e63bd4dc438017d723548
W.G.Zhang <1243271316@qq.com>
2020-11-12 10:33:14 +0800

    finished Exercise06''

--- src/com/zhangweiguang/algorithms4/chapter1_1/Exercise06.java
+++ src/com/zhangweiguang/algorithms4/chapter1_1/Exercise06.java
@@ -1,12 +1,12 @@
 package com.zhangweiguang.algorithms4.chapter1_1;
 
 import edu.princeton.cs.algs4.StdOut;
-//Fibonacci sequence
+//Fibonacci sequence xian
 public class Exercise06 {
     public static void main(String[] args)
     {
         int f=0,g=1;
-        for(int i=0;i<=25;i++)
+        for(int i=0;i<=20;i++)
         {
             StdOut.println(f);
             f=f+g;
