5ee92bb23cb90a221fd22638e67a3191c8f92906
W.G.Zhang <1243271316@qq.com>
2020-11-12 11:07:38 +0800

    finished Exercise07

---
+++ src/com/zhangweiguang/algorithms4/chapter1_1/Exercise07.java
@@ -0,0 +1,16 @@
+package com.zhangweiguang.algorithms4.chapter1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise07 {
+    public static void main(String[] args)
+    {
+        int f=0,g=1;
+        for(int i=0;i<=25;i++)
+        {
+            StdOut.println(f);
+            f=f+g;
+            g=f-g;
+        }
+    }
+}
